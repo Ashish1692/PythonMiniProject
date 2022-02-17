6ae4b9a7e2af5fb04021559a30c5e495ccc2615c
Ashish Moghe <ashishmoghe6@gmail.com>
2022-02-17 20:28:22 +0530

    repo initialize

---
+++ all_imports.py
@@ -0,0 +1,10 @@
+import os
+import sys
+from platform import system
+from PyQt5 import QtGui
+from PyQt5 import uic
+from PyQt5.QtCore import Qt, QTimer
+from PyQt5.QtGui import QPalette, QColor, QPixmap
+from PyQt5.QtWidgets import QInputDialog, QLineEdit, QStyleFactory, QDesktopWidget
+from PyQt5.QtWidgets import QMainWindow, QApplication, QMessageBox, QFileDialog, QSplashScreen
+import main
