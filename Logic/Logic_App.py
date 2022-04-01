76c6b977d572b94b126c63ef25ca236d669292b8
Ashish Moghe <ashishmoghe6@gmail.com>
2022-04-01 12:29:11 +0530

    added new features logic

--- Logic/Logic_App.py
+++ Logic/Logic_App.py
@@ -1,6 +1,8 @@
-from PIL import Image  # pillow library
 import os
 import platform
+
+from PIL import Image  # pillow library
+
 
 def make_pdf_all(dir_path):
     if platform.system() == "Windows":
@@ -25,6 +27,7 @@ def make_pdf_only_selected(selected_files, file_name, pdf_location):
 
 def make_doc_to_pdf(selected_file, pdf_location, pdf_name):
     import os
+
     from docx2pdf import convert
 
     dir_name = pdf_location
@@ -35,6 +38,21 @@ def make_doc_to_pdf(selected_file, pdf_location, pdf_name):
 
     convert(selected_file, output_file)
 
+
+def convert_pdf2doc(selected_file, pdf_location, pdf_name):
+    import os
+    from pdf2docx import Converter
+
+    dir_name = pdf_location
+    base_filename = str(pdf_name)
+    output_file = os.path.join(dir_name, base_filename)
+
+    pdf_file = selected_file
+    docx_file = output_file
+
+    cv = Converter(pdf_file)
+    cv.convert(docx_file)
+    cv.close()
 
 if __name__ == '__main__':
     print("This Is The Main Backend Module!")
